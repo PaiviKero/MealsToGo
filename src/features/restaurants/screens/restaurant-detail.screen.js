23096206803ef7ec5fc0dce0f2da2cf1a15ebf47
Päivi Kero <paivi.kero@gmail.com>
2022-04-20 12:58:40 +0300

    eslint and prettier fixes

--- src/features/restaurants/screens/restaurant-detail.screen.js
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -5,7 +5,7 @@ import { List } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 
-export const RestaurantDetailScreen = ({ route }) => {  
+export const RestaurantDetailScreen = ({ route }) => {
   const [breakfastExpanded, setBreakfastExpanded] = useState(false);
   const [lunchExpanded, setLunchExpanded] = useState(false);
   const [dinnerExpanded, setDinnerExpanded] = useState(false);
