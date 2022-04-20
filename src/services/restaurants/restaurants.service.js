ee0c03a52dc4107a3b7323f117f34b4e72fea2e7
Päivi Kero <paivi.kero@gmail.com>
2022-04-20 12:46:48 +0300

    fixed the NaN keys

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -20,7 +20,7 @@ export const restaurantTransform = ({ results = [] }) => {
       isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
       isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
       address: restaurant.vicinity,
-      key: restaurant.placeId + Math.random(),
+      key: restaurant.place_id,
     };
   });
   //console.log(camelize(mappedResults));
