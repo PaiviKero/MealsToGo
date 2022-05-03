c28a887f4b2c529925fdfb111b78b76a5f165b58
Päivi Kero <paivi.kero@gmail.com>
2022-05-03 11:35:40 +0200

    fixed infrastructure folder name

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components/native";
 import { Button, TextInput } from "react-native-paper";
 
-import { colors } from "../../../infrastucture/theme/colors";
+import { colors } from "../../../infrastructure/theme/colors";
 import { Text } from "../../../components/typography/text.component";
 
 export const AccountBackground = styled.ImageBackground.attrs({
