98891c14f69b8340841fd8b204086527182bdd24
Päivi Kero <paivi.kero@gmail.com>
2022-05-09 12:49:20 +0200

    lint fix

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState } from "react";
 import * as firebase from "firebase/compat";
 
 import { loginRequest } from "./authentication.service";
