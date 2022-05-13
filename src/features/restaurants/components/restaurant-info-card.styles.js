4e715b594f850f453ab51977c583d256f7a5d1ec
Päivi Kero <paivi.kero@gmail.com>
2022-05-13 15:07:16 +0200

    restaurant screen improvements

--- src/features/restaurants/components/restaurant-info-card.styles.js
+++ src/features/restaurants/components/restaurant-info-card.styles.js
@@ -4,6 +4,8 @@ import { Card } from "react-native-paper";
 export const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
   margin-bottom: ${(props) => props.theme.space[3]};
+  align-self: center;
+  width: 95%;
 `;
 
 export const RestaurantCardCover = styled(Card.Cover)`
